c224d3815462db234b12f90920f5f9eb5ca75ab3
CMK <ckrnke@gmail.com>
2019-10-17 12:22:08 -0500

    added backgrounds and more info to home page

--- client/src/components/Hamburger/index.js
+++ client/src/components/Hamburger/index.js
@@ -11,9 +11,9 @@ class Hamburger extends React.Component {
     return (
       <Menu right>
         <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="about" className="menu-item" href="/about">Settings</a>
         <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Log Out</a>
       </Menu>
     );
   }
