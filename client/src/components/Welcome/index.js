2f28efdb7967d9b316555470c8ca303ce5c61424
CMK <ckrnke@gmail.com>
2019-10-17 09:50:00 -0500

    home page being worked on, added some stuff

---
+++ client/src/components/Welcome/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Welcome.css";
+
+function Welcome (){
+    return(
+        <div><span className="welcome">PLACEHOLDER</span></div>
+    )
+}
+
+export default Welcome;
