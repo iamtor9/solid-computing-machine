fcd2c83ae6be07d35ed054d049f919a998d3e2ac
Jason Ostergren <jason@stp-er-10-129-194-224.wireless.umn.edu>
2019-10-18 11:58:06 -0500

    updated styling for notfound and settings pages

---
+++ client/src/components/Settings/index.js
@@ -0,0 +1,55 @@
+import React from "react"
+import "../Settings/Settings.css"
+
+function Settings () {
+return (
+
+<div className="settings1">
+
+<div className="updatePassword">
+    <h1>
+        <label for="updatePassword">Change Password</label> 
+    </h1>
+</div>
+  
+<form className="passWordUpdate">
+
+    <label for="newPassword"></label>
+    <input type="text" id="npass" name="newPassword" placeholder="New Password.."></input>
+
+    <label for="confirmPassword"></label>
+    <input type="text" id="cpass" name="confirmPassword" placeholder="Confirm New Password.."></input>
+
+    <input type="submit" className="btn1" value="Submit"></input>
+
+ 
+
+</form>
+  
+<div className="newContact">
+    <h1>
+        <label for="newContact">Add Emergency Contact</label> 
+    </h1>
+</div>
+
+<form className="addNewContact">
+
+    <label for="fname"></label>
+    <input type="text" id="fname" name="firstname" placeholder="first name.."></input>
+    
+    <label for="lname"></label>
+    <input type="text" id="lname" name="lastname" placeholder="last name.."></input>
+    
+    <label for="phone"></label>
+    <input type="text" id="phone" name="" placeholder="number.."></input>
+    
+    <input type="submit" className="btn1" value="Submit"></input>
+
+  </form>
+</div>
+)
+}
+
+
+
+export default Settings;
