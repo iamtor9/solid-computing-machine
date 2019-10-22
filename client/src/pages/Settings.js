d453e97b1955facdfd8e68e6c282eb7057a31a7b
Jason Ostergren <jason@stp-er-10-129-144-74.wireless.umn.edu>
2019-10-22 12:01:33 -0500

    updated pages/Settings.js with correct component view

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -5,36 +5,9 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 
 
-
-export default class PasswordUpdate extends React.Component {
-    state = {
-      opass: '',
-      npass: '',
-      cpass: ''
-    }
   
-    handleChange = event => {
-      this.setState({ name: event.target.value });
-    }
-  
-    handleSubmit = event => {
-      event.preventDefault();
-  
-      const user = {
-        opass: this.state.opass,
-        npass: this.state.npass,
-        cpass: this.state.cpass
-      };
-  
-      axios.post(``, { user })
-        .then(res => {
-          console.log(res);
-          console.log(res.data);
-        })
-    }
-  
-    render() {
-      return (
+function updateSettings() {
+    return (
         
 <>
 
@@ -43,6 +16,8 @@ export default class PasswordUpdate extends React.Component {
 <Footer />
 
 </>
-      )
-    }
-  }
+
+)
+}
+
+export default updateSettings;
