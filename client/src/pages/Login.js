b2ff28d78b9ea54a446c5432fe70f590d0ca008c
CMK <ckrnke@gmail.com>
2019-10-22 13:33:59 -0500

    have a background set if the screen is too large on load

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import LoginHeader from "../components/LoginHeader";
 import Form from "../components/Form";
 import Footer from "../components/Footer";
+import Overlay from "../components/Overlay";
 
+
+const isMobile = window.innerWidth <= 450;
 
 
 
 function Login ({history}){
-    return(
-        <>
-        <LoginHeader />
-        <Form history={history}/>
-        <Footer />
-        </>
-    )
+    if(!isMobile){
+        return(
+            <>
+            <Overlay />
+            </>
+        )
+    }
+    else{
+        return(
+            <>
+            <LoginHeader />
+            <Form history={history}/>
+            <Footer />
+            </>
+        )
+    }
 }
 
 export default Login;
