8ba37c9d7070f567f669abd1c6d6ef82a9457f62
iamtor9 <iamtor9@gmail.com>
2019-10-22 13:17:00 -0500

    torwork

---
+++ client/src/components/Add/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Add.css";
+
+function Add (){
+    return (
+        <button className="addTime">Add 5 Minutes</button>
+    )
+}
+
+export default Add;
--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios"
 import "../Settings/settings.css";
 
-function Settings() {
-  return (
+
+export default function formSubmit () {
+
+    const [opass, setOpass] = useState("")
+    const [npass, setNpass] = useState("")
+    const [cpass, setCpass] = useState("")
+    const [fname, setFname] = useState("")
+    const [lname, setLname] = useState("")
+    const [mobile, setMobile] = useState("")
+
+ return (
+
     <div className="settings1">
       <div className="updatePassword">
         <h1>
@@ -16,7 +27,9 @@ function Settings() {
         <input
           type="text"
           id="opass"
+          value= {opass}
           name="oldPassword"
+          onChange={(event) => setOpass(event.target.value)}
           placeholder="Old Password"
         ></input>
 
@@ -24,7 +37,10 @@ function Settings() {
         <input
           type="text"
           id="npass"
+          value= {npass}
           name="newPassword"
+          onChange={(event) => setNpass(event.target.value)}
+
           placeholder="New Password"
         ></input>
 
@@ -32,11 +48,12 @@ function Settings() {
         <input
           type="text"
           id="cpass"
+          value= {cpass}
           name="confirmPassword"
+          onChange={(event) => setCpass (event.target.value)}
           placeholder="Confirm New Password"
         ></input>
 
-        <input type="submit" className="btn1" value="Submit"></input>
       </form>
 
       <div className="newContact">
@@ -50,7 +67,9 @@ function Settings() {
         <input
           type="text"
           id="fname"
+          value= {fname}
           name="firstname"
+          onChange={(event) => setFname (event.target.value)}
           placeholder="First Name"
         ></input>
 
@@ -58,17 +77,44 @@ function Settings() {
         <input
           type="text"
           id="lname"
+          value= {lname}
           name="lastname"
+          onChange={(event) => setLname (event.target.value)}
           placeholder="Last Name"
         ></input>
 
         <label for="phone"></label>
-        <input type="text" id="phone" name="" placeholder="Mobile Number"></input>
+        <input 
+          type="text" 
+          id="phone"
+          value= {mobile}
+          name="mobile"
+          onChange={(event) => setMobile (event.target.value)} 
+          placeholder="Mobile Number"></input>
 
-        <input type="submit" className="btn1" value="Submit"></input>
-      </form>
+        <button type="submit" className="btn1" value="Submit" onClick={event => {
+          // Preventing the default behavior of the form submit (which is to refresh the page)
+          event.preventDefault();
+
+          alert("Hello");
+          setOpass("");
+          setNpass("");
+          setCpass("");
+          setFname("");
+          setLname("");
+          setMobile("");
+          console.log(opass)
+          console.log(npass)
+          console.log(cpass)
+          console.log(fname)
+          console.log(lname)
+          console.log (mobile)
+
+
+        }}>Submit</button>      
+        </form>
     </div>
   );
 }
 
-export default Settings;
+
