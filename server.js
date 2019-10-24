e6599cf0a22d5ff9b2418ca30f0b5627772206cf
Adam Cooper <frankidyfrank3@gmail.com>
2019-10-24 11:27:29 -0500

    fgsdfgsdf

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path = require("path")
+const path = require("path");
 const app = express();
 
-
 const PORT = process.env.PORT || 8080;
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,15 +15,13 @@ if (process.env.NODE_ENV === "production") {
 
 routes(app);
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 /*React root*/
 app.get("*", (req, res) => {
-res.sendFile(path.join(__dirname + "../client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/safeDB");
-
 
 app.listen(PORT, function() {
   console.log(`API Server now listening on PORT ${PORT}!`);
