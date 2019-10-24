f625abf3484fde7775ba66b191370c37e42a3666
Adam Cooper <frankidyfrank3@gmail.com>
2019-10-24 09:52:18 -0500

    fgsdfgsdf

--- routes/forOfor.js
+++ routes/forOfor.js
@@ -3,17 +3,17 @@ const router = express.Router();
 
 //this is a test to prove my point that the token validation works
 router.post("*", function(req, res) {
-  res.sendFile("../client/public/index.html");
+  res.status(404).json({ message: "route not found" });
 });
 
 //this is a test to prove my point that the token validation works
 router.get("*", function(req, res) {
-  res.sendFile("../client/public/index.html");
+  res.status(404).json({ message: "route not found" });
 });
 
 //this is a test to prove my point that the token validation works
 router.delete("*", function(req, res) {
-  res.sendFile("../client/public/index.html");
+  res.status(404).json({ message: "route not found" });
 });
 
 module.exports = router;
