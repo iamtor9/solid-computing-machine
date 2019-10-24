1b1fd429cbe9688e313d9d6f5d360a2e16fad103
CMK <ckrnke@gmail.com>
2019-10-24 10:49:34 -0500

    have the coords and map set, still need some tweaking to make more robust

--- client/src/components/Maps/index.js
+++ client/src/components/Maps/index.js
@@ -1,56 +1,42 @@
-import React, {Component } from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
-import Dot from "./Dot"
+import Marker from "./Marker";
 import "./Maps.css";
+
+const Maps = () => {
+
  
+    let [center, setCenter] = useState({lat: 44.9778, lng: -93.2650 });
+    let [zoom, setZoom] = useState(16);
+    let [lat, setLat] = useState(44.9778);
+    let [lng, setLng] = useState(-93.2650);
 
-
-class Maps extends Component {
-   constructor(){
-      super();
-      this.state = {
-        ready: false,
-        where: {lat:null, lng:null},
-        error: null,
-        zoom: 15
-    };
-   }
-  componentDidMount(){
-      let geoOptions ={
-        enableHighAccuracy: true,
-        timeOut: 20000,
-        maximunAge: 60 * 60,
-        distanceFilter: 1
-      };
-      this.setState({ready: false, error: null})
-      navigator.geolocation.getCurrentPosition(this.geoSuccess, this.geoFailure, geoOptions);
-  }
-  geoSuccess = (position) => {
-    this.setState({
-      ready: true,
-      where: {lat: position.coords.latitude, lng: position.coords.longitude}
+    navigator.geolocation.watchPosition((position) =>{
+      setCenter({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+      setLat(position.coords.latitude);
+      setLng(position.coords.longitude);
+      console.log(position);
     })
-    console.log(position);
-  }
-  geoFailure = (err) =>{
-    this.setState({error: err.message});
-  }
- 
-  render() {
-    // console.log(this.state);
+
     return (
-      
-      <div className="theMap" style={{ height: '30vh', width: '100vw' }}>
+        <div className="theMap" style={{ height: '30vh', width: '100vw' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAvh-RJE3-FnbTJlwKg-npCYZl_Yo8P6RU"}}
-          defaultCenter={this.state.where}
-          defaultZoom={this.state.zoom}
+          bootstrapURLKeys={{ key: 'AIzaSyAvh-RJE3-FnbTJlwKg-npCYZl_Yo8P6RU' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
         >
-          <Dot />
+          <Marker
+            lat={lat}
+            lng={lng}
+            name="My Marker"
+            color="red"
+          />
         </GoogleMapReact>
       </div>
     );
-  }
 }
- 
+
 export default Maps;
